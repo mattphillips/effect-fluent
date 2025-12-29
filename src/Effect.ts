f789f374f0e7e507c8c656d8c42e57046bdb6a9a
Matt Phillips <matt@mattphillips.io>
2025-12-29 11:56:11 +0000

    wip: add with apis

--- src/Effect.ts
+++ src/Effect.ts
@@ -1,5 +1,8 @@
-import { Effect as _Effect, type Cause, type FiberId } from 'effect';
+import { Effect as _Effect, pipe, type Cause, type FiberId } from 'effect';
 import type { LazyArg } from 'effect/Function';
+import { SingleShotGen, YieldWrap } from 'effect/Utils';
+
+type Test = typeof _Effect;
 
 export class Effect<A, E = never, R = never> {
   static of<A, E = never, R = never>(effect: _Effect.Effect<A, E, R>): Effect<A, E, R> {
@@ -78,4 +81,322 @@ export class Effect<A, E = never, R = never> {
   with<A1, E1, R1>(f: (effect: _Effect.Effect<A, E, R>) => _Effect.Effect<A1, E1, R1>): Effect<A1, E1, R1> {
     return new Effect(f(this.value));
   }
+
+  with2<A1, E1, R1>(f: (effect: _Effect.Effect<A, E, R>, _: Test) => _Effect.Effect<A1, E1, R1>): Effect<A1, E1, R1> {
+    return new Effect(f(this.value, _Effect));
+  }
+
+  with3<A1, E1, R1>(
+    f: (effect: _Effect.Effect<A, E, R>) => (_: Test) => _Effect.Effect<A1, E1, R1>
+  ): Effect<A1, E1, R1> {
+    return new Effect(f(this.value)(_Effect));
+  }
+
+  with4<A1, E1, R1>(
+    f: (_: Test) => (effect: _Effect.Effect<A, E, R>) => _Effect.Effect<A1, E1, R1>
+  ): Effect<A1, E1, R1> {
+    return new Effect(f(_Effect)(this.value));
+  }
+
+  static gen<Eff extends YieldWrap<Effect<any, any, any>>, AEff>(
+    f: (resume: Adapter) => Generator<Eff, AEff, never>
+  ): Effect<
+    AEff,
+    [Eff] extends [never] ? never : [Eff] extends [YieldWrap<Effect<infer _A, infer E, infer _R>>] ? E : never,
+    [Eff] extends [never] ? never : [Eff] extends [YieldWrap<Effect<infer _A, infer _E, infer R>>] ? R : never
+  > {
+    // new Effect(_Effect.gen(() => f(pipe)))
+
+    return null as any;
+  }
+
+  [Symbol.iterator](): EffectGenerator<Effect<A, E, R>> {
+    return new SingleShotGen(new YieldWrap(this));
+  }
+}
+
+export type Success<T extends Effect<any, any, any>> = [T] extends [Effect<infer _A, infer _E, infer _R>] ? _A : never;
+
+export interface EffectGenerator<T extends Effect<any, any, any>> {
+  next(...args: ReadonlyArray<any>): IteratorResult<YieldWrap<T>, Success<T>>;
+}
+
+const blah = Effect.gen(function* () {
+  const one = yield* Effect.succeed(1);
+
+  return one;
+});
+
+const blah2 = _Effect.gen(function* () {
+  const one = yield* _Effect.succeed(1);
+});
+
+const double = (n: number) => n * 2;
+
+const x = Effect.succeed(1)
+  .with((self) => _Effect.map(self, double))
+  .with2((e, _) => _.map(e, double))
+  .with3((e) => (_) => _.map(e, double))
+  .with4((_) => _.map(double));
+
+export interface Adapter {
+  <A, E, R>(self: Effect<A, E, R>): Effect<A, E, R>;
+  <A, _A, _E, _R>(a: A, ab: (a: A) => Effect<_A, _E, _R>): Effect<_A, _E, _R>;
+  <A, B, _A, _E, _R>(a: A, ab: (a: A) => B, bc: (b: B) => Effect<_A, _E, _R>): Effect<_A, _E, _R>;
+  <A, B, C, _A, _E, _R>(a: A, ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => Effect<_A, _E, _R>): Effect<_A, _E, _R>;
+  <A, B, C, D, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (g: H) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => O,
+    op: (o: O) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => O,
+    op: (o: O) => P,
+    pq: (p: P) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => O,
+    op: (o: O) => P,
+    pq: (p: P) => Q,
+    qr: (q: Q) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => O,
+    op: (o: O) => P,
+    pq: (p: P) => Q,
+    qr: (q: Q) => R,
+    rs: (r: R) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => O,
+    op: (o: O) => P,
+    pq: (p: P) => Q,
+    qr: (q: Q) => R,
+    rs: (r: R) => S,
+    st: (s: S) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, _A, _E, _R>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+    jk: (j: J) => K,
+    kl: (k: K) => L,
+    lm: (l: L) => M,
+    mn: (m: M) => N,
+    no: (n: N) => O,
+    op: (o: O) => P,
+    pq: (p: P) => Q,
+    qr: (q: Q) => R,
+    rs: (r: R) => S,
+    st: (s: S) => T,
+    tu: (s: T) => Effect<_A, _E, _R>
+  ): Effect<_A, _E, _R>;
 }
